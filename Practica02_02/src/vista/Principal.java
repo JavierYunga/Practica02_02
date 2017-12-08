26d00160a4dd508084825d9ef63faa3374bad26b
USER <USER@christian>
2017-12-08 15:37:29 -0500

    Esta creada la ventana Curso y su respectivo Evento

--- Practica02_02/src/vista/Principal.java
+++ Practica02_02/src/vista/Principal.java
@@ -28,8 +28,20 @@ public class Principal {
         List<Docente> dL = new ArrayList<Docente>();
         List<Materia> mL = new ArrayList<Materia>();
         List<Curso> cL = new ArrayList<Curso>();
-        File archivo = new File("C:\\Archivo");
-        GestionDato gD = new GestionDato(dL, mL, cL);
+       
+        Docente d1=new Docente(101,"Pepe Sanchez","0102456789");
+        Docente d2=new Docente(102,"Juan Diaz","0102426719");
+       
+        Materia m1=new Materia("Matematicas",120,130);
+        Materia m2=new Materia("Programacion",160,120);
+       
+        
+        dL.add(d1);
+        dL.add(d2);
+        mL.add(m1);
+        mL.add(m2);
+        
+        GestionDato gD = new GestionDato(dL, mL,cL);
         VentanaPrincipal v = new VentanaPrincipal("Archivos de Texto",gD);
         v.setVisible(true);
         
